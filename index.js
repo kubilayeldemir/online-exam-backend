9e5e2791cb9269eeb44c07a29e62955a513025c8
KUBİLAY ELDEMİR <kubilayeldemir@hotmail.com>
2021-01-30 18:55:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+app.use(express.json());
+
+
+const courses = [{
+        id: 1,
+        name: "course1"
+    },
+    {
+        id: 2,
+        name: "course2"
+    },
+    {
+        id: 3,
+        name: "course3"
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course){
+        res.status(404).send('Course bulunamadı');
+    }
+    res.send(course);
+})
+
+app.post('/api/courses',(req,res)=>{
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body,schema);
+    console.log(result);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id:courses.length + 1,
+        name:req.body.name,
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) {
+        res.status(404).send('Course bulunamadı');
+    }
+    const { error } = validaCourse(req.body);//result.error
+    
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    course.name=req.body.name;
+    res.send(course);
+
+});
+
+app.delete("/api/courses/:id",(req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
+})
+
+function validaCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return  Joi.validate(course, schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
